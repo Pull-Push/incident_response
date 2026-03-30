55182f5393d80a23833a354d79ef00335268e276
Jeff Sokol <JeffSokolDev@gmail.com>
2026-03-30 18:27:06 -0400

    login page complete

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,72 @@
+import { logIn } from "../services/api";
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
+
+export default function Login(){
+    const [error, setError] = useState(null)
+    const [submitting, setSubmitting] = useState(false)
+    const [form, setForm ] = useState({
+        email: '',
+        password: '',
+    })
+    const navigate = useNavigate()
+
+    const handleChange = (e) => {
+        const { name, value, type, checked } = e.target
+        setForm(prev => ({ ...prev, [name]: type === 'checkbox' ? checked : value }))
+    }
+
+        const handleSubmit = async (e) => {
+            e.preventDefault()
+            setError(null);
+            setSubmitting(true);
+            try {
+                const data = await logIn(form)
+                localStorage.setItem('token', data.token)
+                navigate('/dashboard')
+            } catch (err) {
+                setError('Failed to Log In. Please try again.')
+                console.error(err)
+                resetForm()
+            } finally {
+                setSubmitting(false)
+            }
+        }
+        const resetForm = () =>{
+            setForm({
+                email: '',
+                password: ''
+            })
+        }
+    return(
+        <div className="page-main">
+            <div className="page-content">
+                <div className="page-upper">
+                    <img src="" alt="ICR LOGO" />
+                </div>
+                <div className="page-lower">
+                    {error && <div className="error-banner">{error}</div>}
+                    <form className="form-card" onSubmit={handleSubmit}>
+                    <div className="form-section">
+                        <h1>Log In</h1>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" name="email" id="email" required value={form.email} onChange={handleChange}/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" name="password" id="password" required value={form.password} onChange={handleChange}/>
+                        </div>
+                        <div className="form-actions">
+                            <button type="submit" className="btn btn-primary" disabled={submitting}>
+                                {submitting ? 'Logging In...' : 'Log In'}
+                            </button>
+                        </div>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
