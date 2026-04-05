ce3f5247e776b255e871a47c500b0c07cfdcd946
Jeff Sokol <JeffSokolDev@gmail.com>
2026-04-05 11:21:00 -0400

    subsite service file completion

---
+++ server/services/subsiteService.js
@@ -0,0 +1,81 @@
+const pool = require('../db')
+
+
+//GET SUBSITES (need customer ID)
+async function getSubsites(customer_id) {
+    try {
+        const result = await pool.query(
+            `SELECT *
+            FROM subsites
+            WHERE customer_id = $1 AND is_valid = true`,
+            [customer_id]
+        )
+        return result.rows
+    } catch (error) {
+        console.error('Failed to get customer subsites')
+        throw error
+    }
+}
+
+//CREATE SUBSITE
+async function insertSubsite(subsite_info) {
+    try {
+        const { customer_id, address, city, state, zip, notes, contact, phone, lat, long, name  } = subsite_info
+        const result = await pool.query(
+            `INSERT INTO subsites (customer_id, address, city, state, zip, notes, contact, phone, lat, long, name)
+            VALUES ($1, $2,$3,$4,$5,$6,$7,$8,$9,$10,$11)
+            RETURNING *`,
+            [customer_id, address, city, state, zip, notes, contact, phone, lat, long, name]
+        )
+        return result.rows[0]
+    } catch (error) {
+        console.error("failed to create subsite")
+        throw error
+    }
+}
+
+//UPDATE SUBSITE
+async function updateSubsite(subsite_id, update_info) {
+    try {
+        const { customer_id, address, city, state, zip, notes, contact, phone, lat, long, name  } = update_info
+        const result = await pool.query(
+            `UPDATE subsites
+            SET customer_id = COALESCE($1, customer_id),
+                address = COALESCE($2, address),
+                city = COALESCE($3, city),
+                state = COALESCE($4, state),
+                zip = COALESCE($5, zip),
+                notes = COALESCE($6, notes),
+                contact = COALESCE($7, contact),
+                phone = COALESCE($8, phone),
+                lat = COALESCE($9, lat),
+                long = COALESCE($10, long),
+                name = COALESCE($11, name)
+            WHERE id = $12
+            RETURNING *`,
+            [customer_id, address, city, state, zip, notes, contact, phone, lat, long, name, subsite_id]
+        ) 
+        return result.rows[0]
+    } catch (error) {
+        console.error('Failed to update subsite')
+        throw error
+    }
+}
+//SOFT DELETE SUBSITE - need subsite id - is_valid = false
+async function deactivateSubsite(subsite_id) {
+    try {
+        const result = await pool.query(
+        `UPDATE subsites
+        SET is_valid = false
+        WHERE id = $1
+        RETURNING *`,
+        [subsite_id]
+    )
+    return result.rows[0]
+    } catch (error) {
+        console.error('Failed to delete subsite')
+        throw error
+    }
+}
+
+module.exports = { getSubsites, insertSubsite, updateSubsite, deactivateSubsite}
